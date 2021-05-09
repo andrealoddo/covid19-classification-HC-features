b3f42392e63927b34e3a71301949c3973556a97f
Snarci <58110072+Snarci@users.noreply.github.com>
2021-05-09 14:36:35 +0200

    feature extraction parallelization on cpu

--- code/extractTableFromImgDatastore.m
+++ code/extractTableFromImgDatastore.m
@@ -1,6 +1,6 @@
 function [outputTable] = extractTableFromImgDatastore(inputDatastore,descriptor)
-%EXTRACTTABLEFROMIMGDATASTORE Create ml table from dataset
-%   Detailed explanation goes here
+%EXTRACTTABLEFROMIMGDATASTORE Create ml suitable table from dataset
+%Tramite calcolo parallelo dimezzo(circa) il tempo necessario
 %creo array di laber e features che serviranno per comporre la tabella
 labels=[];
 features=[];
@@ -8,11 +8,34 @@ color = 'gray';
 graylevel = 256;
 prepro = 'none';
 %popolo gli array di features e labels concatenando ad ogni iterazione
-while hasdata(inputDatastore)
+%controllo se posso usare il calcolo in parallelo
+if not(isempty(ver('parallel')))  
+    %suddivido il carico delle partizioni tra più workers
+    n = numpartitions(inputDatastore,gcp); 
+    %parallel for per utilizzare i diversi workers
+    parfor ii = 1:n
+        labelsin=[];
+        featuresin=[];
+        subds = partition(inputDatastore,n,ii);
+          while hasdata(subds)
+            [element,info]=read(subds);
+            %questi due array sono temporanei, è importante che vengano
+            %dichiarati all'interno del parfor e ad ogni iterazione vengono
+            %reinizializzati
+            labelsin=cat(1,labelsin,info.Label');
+            featuresin=cat(1,featuresin,featureExtraction(element,descriptor,color, graylevel, prepro).');
+
+          end
+          %vertcat unica soluzione non concateva col cat tradizionale
+          labels=vertcat(labels,labelsin);
+          features=vertcat(features,featuresin);
+    end
+else  
+    %non avendo a disposizione il pct si intera in maniera classica 
+    while hasdata(inputDatastore)
     [element,info]=read(inputDatastore);
     labels=cat(1,labels,info.Label);
     features=cat(1,features,featureExtraction(element, descriptor, color, graylevel, prepro).');
+    end
 end
 outputTable=table(labels,features);
-end
-
--- code/mainScript.m
+++ code/mainScript.m
@@ -24,18 +24,18 @@ features = featureExtraction(img, descriptor, color, graylevel, prepro);
 [trainHAR,validateHAR]=extractFeaturesAndSplit('HAR',0.8);
 [trainLBP18,validateLBP18]=extractFeaturesAndSplit('LBP18',0.8);
 
-[trainLBP18e,validateLBP18e,testLBP18e]=ettExtractFeaturesAndSplit('LBP18');
-[trainHARe,validateHARe,testHARe]=ettExtractFeaturesAndSplit('HAR');
+%[trainLBP18e,validateLBP18e,testLBP18e]=ettExtractFeaturesAndSplit('LBP18');
+%[trainHARe,validateHARe,testHARe]=ettExtractFeaturesAndSplit('HAR');
 %eseguo il predict tramite il modello 
 %essendo il dataset ancora di piccole dimensioni i risultati possono essere
 %falsati, aumentando il fold è possibile ottenere risultati più concreti
 %prima di eseguire questa porzione è necessario avere un modello già
 %allenato
-predictions = trainedModel.predictFcn(validateHAR);
+    %predictions = trainedModel.predictFcn(validateHAR);
 %calcolo l'accuratezza
 
-iscorrect=predictions==validateHAR.labels;
-iscorrect=iscorrect(:,1);
-sizeArray=size(trainLBP18);
-accuracy=sum(iscorrect)*100/sizeArray(1);
+    %iscorrect=predictions==validateHAR.labels;
+    %iscorrect=iscorrect(:,1);
+    %sizeArray=size(trainLBP18);
+    %accuracy=sum(iscorrect)*100/sizeArray(1);
 
