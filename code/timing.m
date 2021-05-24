ab9f874ac770ce06fe646611fbd3b77a0a5dc2f3
Snarci <58110072+Snarci@users.noreply.github.com>
2021-05-24 15:36:19 +0200

    pt 2

--- code/timing.m
+++ code/timing.m
@@ -1,6 +1,8 @@
-trainClassifiersAndReport();
+%trainClassifiersAndReport();
 
-%a=utilityClassifiers("accuracy",10);
+a=utilityClassifiers("accuracy",10);
 %b=utilityClassifiers("accuracy");
 %c=utilityClassifiers("time",10);
 %d=utilityClassifiers("time");
+
+%tr=utilityClassifiers("vote");
